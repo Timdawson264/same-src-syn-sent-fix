011567c42fa079f454f64f1456b34a92a613851b
Tim Dawson <tidawson@redhat.com>
2022-07-01 17:22:02 +1200

    Working DS

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Deploying
+
+```
+./build.sh 4.10.16 | oc apply -n openshift-sdn -f -
+```
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 
 OCP_VER="$1"
 
 IMAGE="$( oc adm release info ${OCP_VER} --image-for=sdn )"
 
-cat ds.yaml | sed -e "s|image:.*|image: $IMAGE|" > ds-$OCP_VER.yaml
+cat ds.yaml | sed -e "s|<IMAGE>|$IMAGE|" 
